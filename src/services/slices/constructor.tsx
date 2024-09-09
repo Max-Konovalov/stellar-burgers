8f63cc38740dbbfa58b0bf6ca319f9036d9fb474
mkonoval0v <m.konovalov@brighttech.ru>
2024-09-09 15:17:02 +0300

    review#1

---
+++ src/services/slices/constructor.tsx
@@ -0,0 +1,53 @@
+import {
+  PayloadAction,
+  createAsyncThunk,
+  createSlice,
+  nanoid
+} from '@reduxjs/toolkit';
+import { TConstructorIngredient, TIngredient } from '@utils-types';
+
+type TConstructorState = {
+  bun: TConstructorIngredient | null;
+  ingredients: TConstructorIngredient[];
+};
+
+const initialState: TConstructorState = {
+  bun: null,
+  ingredients: []
+};
+
+export const constructorSlice = createSlice({
+  name: 'constructorIngredient',
+  initialState,
+  reducers: {
+    addItem: {
+      reducer: (state, action: PayloadAction<TConstructorIngredient>) => {
+        if (action.payload.type === 'bun') {
+          state.bun = action.payload;
+        } else {
+          state.ingredients.push(action.payload);
+        }
+      },
+      prepare: (ingredient: TConstructorIngredient) => {
+        const key = nanoid();
+        return { payload: { ...ingredient, key } };
+      }
+    },
+    deleteItem: (state, action: PayloadAction<TConstructorIngredient>) => {
+      state.ingredients = state.ingredients.filter(
+        (item) => item.id !== action.payload.id
+      );
+    },
+    clearAll: (state) => (state = initialState),
+    updateAll: (state, action: PayloadAction<TConstructorIngredient[]>) => {
+      state.ingredients = action.payload;
+    }
+  },
+  selectors: {
+    selectItems: (state: TConstructorState) => state
+  }
+});
+
+export const { addItem, deleteItem, clearAll, updateAll } =
+  constructorSlice.actions;
+export const constructorSelector = constructorSlice.selectors;
